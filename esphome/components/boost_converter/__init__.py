7651c2e06c2d3edab226b1f3efbe48d4211bdc9e
Odd Arne Beck <oddarne.beck@yyz.no>
2025-08-03 21:15:06 +0200

    test

--- esphome/components/boost_converter/__init__.py
+++ esphome/components/boost_converter/__init__.py
@@ -1,7 +1,7 @@
 from esphome.components import custom
 import esphome.config_validation as cv
 
-CODEOWNERS = ["@yourgithub"]
+CODEOWNERS = ["@oddbeck"]
 
 # This registers the C++ class so ESPHome knows to compile it
 custom.custom_component_schema(cv.Schema({}))
