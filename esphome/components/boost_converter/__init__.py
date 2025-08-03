cb39e9a7533ed5472110590ddc76692bf1860580
Odd Arne Beck <oddarne.beck@yyz.no>
2025-08-03 21:20:33 +0200

    test

--- esphome/components/boost_converter/__init__.py
+++ esphome/components/boost_converter/__init__.py
@@ -1,7 +1,11 @@
-from esphome.components import custom
+import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.components import custom
+from esphome.const import CONF_ID
 
-CODEOWNERS = ["@oddbeck"]
+BOOST_CONVERTER_ID = cg.declare_id("BoostConverter")
 
-# This registers the C++ class so ESPHome knows to compile it
-custom.custom_component_schema(cv.Schema({}))
+BOOST_CONVERTER_SCHEMA = cv.Schema({})
+
+def to_code(config):
+    cg.add_library('boost_converter')
