eab931cf44c8a2cde5234f8016375b5d1d5cf45f
Odd Arne Beck <oddarne.beck@yyz.no>
2025-08-03 21:24:46 +0200

    test

--- esphome/components/boost_converter/boost_converter.cpp
+++ esphome/components/boost_converter/boost_converter.cpp
@@ -1,9 +1,11 @@
 #include "boost_converter.h"
 
-namespace esphome {
-namespace boost_converter {
+namespace esphome
+{
+    namespace boost_converter
+    {
 
-// Nothing needed here yet
+        // Nothing needed here yet
 
-}  // namespace boost_converter
-}  //
+    } // namespace boost_converter
+} //
