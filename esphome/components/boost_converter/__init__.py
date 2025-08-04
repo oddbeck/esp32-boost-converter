47aee64dcfb85fcf5e31244adc591b357b0beb73
Odd Arne Beck <oddarne.beck@yyz.no>
2025-08-04 20:26:07 +0200

    test

--- esphome/components/boost_converter/__init__.py
+++ esphome/components/boost_converter/__init__.py
@@ -1,11 +1,43 @@
-import esphome.codegen as cg
+# import esphome.codegen as cg
+# import esphome.config_validation as cv
+# from esphome.components import custom
+# from esphome.const import CONF_ID
+
+# BOOST_CONVERTER_ID = cg.declare_id("BoostConverter")
+
+# BOOST_CONVERTER_SCHEMA = cv.Schema({})
+
+# def to_code(config):
+#     cg.add_library('boost_converter')
+
 import esphome.config_validation as cv
-from esphome.components import custom
+import esphome.codegen as cg
+
 from esphome.const import CONF_ID
 
-BOOST_CONVERTER_ID = cg.declare_id("BoostConverter")
+CONF_FOO = "foo"
+CONF_BAR = "bar"
+CONF_BAZ = "baz"
 
-BOOST_CONVERTER_SCHEMA = cv.Schema({})
+example_component_ns = cg.esphome_ns.namespace("BoostConverter")
+BoostConverterComponent = example_component_ns.class_("BoostConverter", cg.Component)
 
-def to_code(config):
-    cg.add_library('boost_converter')
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(BoostConverterComponent),
+    # cv.Required(CONF_FOO): cv.boolean,
+    # cv.Optional(CONF_FOO): cv.boolean,
+    # cv.Optional(CONF_BAR): cv.string,
+    # cv.Optional(CONF_BAZ): cv.int_range(0, 255),
+})
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+
+    await cg.register_component(var, config)
+
+    # cg.add(var.set_foo(config[CONF_FOO]))
+    # if bar := config.get(CONF_BAR):
+    #     cg.add(var.set_bar(bar))
+    # if baz := config.get(CONF_BAZ):
+    #     cg.add(var.set_baz(baz))
